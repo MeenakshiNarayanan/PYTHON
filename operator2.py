847069c1d74f88fac8cbd70a11b42045212e9518
MeenakshiNarayanan <106669125+MeenakshiNarayanan@users.noreply.github.com>
2022-06-01 20:41:22 +0530

    Add files via upload

---
+++ operator2.py
@@ -0,0 +1,10 @@
+a=0
+a += 1
+a = a+1
+print('The value of a is ' , a)
+print("INCREMENTED FOR LOOP")
+for i in range(0,7):
+    print(i)
+print("\nDECREMENTED FOR LOOP")
+for i in range(6, -1, -1):
+    print(i)
---
+++ operator4.py
@@ -0,0 +1,11 @@
+a= float(input('Enter first number: ' ))
+b= float(input('Enter second number: '))
+if a > b:
+    print(a, "is greater")
+else:
+        print(b, "is greater ")
+
+if a < b:
+    print(a, "is smaller")
+else:
+    print(b, "is smaller")
